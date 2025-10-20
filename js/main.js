beeaf28cab0170c7afcb0805d90188dea21aa832
Rustamov Khumoyun <122399191+humoyuntech@users.noreply.github.com>
2025-10-20 21:42:53 +0500

    greensip.ru

---
+++ js/main.js
@@ -0,0 +1,46 @@
+"use strict";
+
+window.addEventListener("load", () => {
+  const box = document.querySelector(".container");
+  const content = document.querySelector(".content");
+  const body = document.querySelector("body");
+  const burger = document.querySelector(".menu__burger");
+  const navbar = document.querySelector(".navbar");
+  const menuClose = document.querySelector(".menu__close");
+  const buttons = document.querySelectorAll(".btn"); // 🔹 barcha btn tugmalar
+
+  function logSizes() {
+    if (!box || !content) return;
+
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    console.clear();
+    console.log("Ekran:", width, "x", height);
+    console.log("body", body.clientWidth, "px");
+    console.log("Container kengligi:", box.clientWidth, "px");
+    console.log("Content kengligi:", content.clientWidth, "px");
+  }
+
+  burger.addEventListener("click", () => {
+    navbar.style.display = "block";
+    burger.style.display = "none";
+    menuClose.style.display = "block";
+  });
+
+  menuClose.addEventListener("click", () => {
+    menuClose.style.display = "none";
+    burger.style.display = "block";
+    navbar.style.display = "none";
+  });
+
+  // 🔹 Barcha tugmalarga event biriktiramiz
+  buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      alert("Backend hali tayyor emas");
+    });
+  });
+
+  logSizes();
+  window.addEventListener("resize", logSizes);
+});
